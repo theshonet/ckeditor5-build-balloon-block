05b7259d2e5049dc8584b59e251a355a45695836
rezonjov <rezonjov@gmail.com>
2019-10-07 15:50:11 +0700

    chore: update icons

--- webpack.config.js
+++ webpack.config.js
@@ -7,61 +7,61 @@
 
 /* eslint-env node */
 
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
+const path = require('path');
+const webpack = require('webpack');
+const { bundler, styles } = require('@ckeditor/ckeditor5-dev-utils');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
 	devtool: 'source-map',
 	performance: { hints: false },
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
 
 	output: {
 		// The name under which the editor will be exported.
 		library: 'BalloonEditor',
 
-		path: path.resolve( __dirname, 'build' ),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
-		libraryExport: 'default'
+		libraryExport: 'default',
 	},
 
 	optimization: {
 		minimizer: [
-			new UglifyJsWebpackPlugin( {
+			new UglifyJsWebpackPlugin({
 				sourceMap: true,
 				uglifyOptions: {
 					output: {
 						// Preserve CKEditor 5 license comments.
-						comments: /^!/
-					}
-				}
-			} )
-		]
+						comments: /^!/,
+					},
+				},
+			}),
+		],
 	},
 
 	plugins: [
-		new CKEditorWebpackPlugin( {
+		new CKEditorWebpackPlugin({
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
 			language: 'en',
-			additionalLanguages: 'all'
-		} ),
-		new webpack.BannerPlugin( {
+			additionalLanguages: 'all',
+		}),
+		new webpack.BannerPlugin({
 			banner: bundler.getLicenseBanner(),
-			raw: true
-		} ),
-		new webpack.NormalModuleReplacementPlugin( /pilcrow\.svg/, './plus-circle-solid.svg' )
+			raw: true,
+		}),
+		new webpack.NormalModuleReplacementPlugin(/pilcrow\.svg/, './plus-solid.svg'),
 	],
 
 	module: {
 		rules: [
 			{
 				test: /\.svg$/,
-				use: [ 'raw-loader' ]
+				use: ['raw-loader'],
 			},
 			{
 				test: /\.css$/,
@@ -69,20 +69,20 @@ module.exports = {
 					{
 						loader: 'style-loader',
 						options: {
-							injectType: 'singletonStyleTag'
-						}
+							injectType: 'singletonStyleTag',
+						},
 					},
 					{
 						loader: 'postcss-loader',
-						options: styles.getPostCssConfig( {
+						options: styles.getPostCssConfig({
 							themeImporter: {
-								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+								themePath: require.resolve('@ckeditor/ckeditor5-theme-lark'),
 							},
-							minify: true
-						} )
+							minify: true,
+						}),
 					},
-				]
-			}
-		]
-	}
+				],
+			},
+		],
+	},
 };
